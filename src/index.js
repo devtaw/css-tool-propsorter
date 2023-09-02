1898b2dc58462e7f45f244d15c3524d0cdd8bb14
Taw Simões <tawm.dev@gmail.com>
2023-09-02 00:06:59 -0300

    :lipstick: feat: Estiliza formatação das mensagens de retorno utilizando Chalk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import chalk from "chalk";
 import { ordenador } from "./ordenador-propriedades-css.js";
 
 function pedePropriedadeCss() {
-  const entradaCss = readline.question(chalk.blue("Insira uma única propriedade CSS ou 'SAIR' para encerrar: "));
+  const entradaCss = readline.question(chalk.white.bgBlue.bold("Insira apenas uma propriedade CSS ou 'SAIR' para encerrar: "));
 
   if (entradaCss.toUpperCase() === "SAIR") {
     ordenador.mostrarPropriedadesCssOrdenadas();
@@ -13,7 +13,7 @@ function pedePropriedadeCss() {
   if (ordenador.validarEntradaCss(entradaCss) === true) {
     ordenador.adicionarPropriedadeCss(entradaCss);
   } else {
-    console.log(chalk.red("ATENÇÃO: entrada inválida! Insira uma única propriedade CSS ou digite 'SAIR'."));
+    console.log(chalk.red.bgYellow.bold("ATENÇÃO: entrada inválida! Insira apenas uma propriedade CSS ou digite 'SAIR'."));
   }
 
   pedePropriedadeCss();
